ff24ab3584a3d92cd24181605f377e30537a5aec
MS-Raunak <raunakkr.saha@gmail.com>
2024-07-15 23:11:06 +0530

    Functional Component

---
+++ 01basic/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import FunctionalComponent from './components/FunctionalComponet'
+
+function App() {
+  
+
+  return (
+    <>
+    <FunctionalComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ 01basic/src/components/FunctionalComponet.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+//Functional Component, using Normal Function
+// function FunctionalComponent() {
+//     return(
+//          <>
+//             <h2>Functional Component</h2>
+//          </>
+//     )
+// }
+
+//Functional Component, using Normal Function
+const FunctionalComponent = ()=>{
+    return(
+        <>
+         <h2>Functional Component</h2>
+       </>
+    )
+}
+
+
+export default FunctionalComponent;

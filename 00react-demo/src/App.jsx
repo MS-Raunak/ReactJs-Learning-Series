b0de335c99fa8861b214196955ab7ee36eeadc65
MS-Raunak <raunakkr.saha@gmail.com>
2024-07-15 23:06:50 +0530

    create first react-app

---
+++ 00react-demo/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>This is my first React Application created by using vite library</p>
+      <p><strong>Command</strong>: npm create vite@latest </p>
+    </div>
+  )
+}
+
+export default App

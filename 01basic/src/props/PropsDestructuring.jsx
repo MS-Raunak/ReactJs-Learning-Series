e837612025af54a14a9a2c02460850f8c8afa9df
MS-Raunak <raunakkr.saha@gmail.com>
2024-07-16 01:24:46 +0530

    props destructuring

---
+++ 01basic/src/props/PropsDestructuring.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function PropsDestructuring({msg}) {
+  return (
+    <div>
+      <h2>{msg}</h2>
+    </div>
+  )
+}
+
+export default PropsDestructuring
+/**
+ * props destructuring:
+ * Basically destructuring is the features of ES6 which is used to
+ * extract the value from array or object. 
+ */

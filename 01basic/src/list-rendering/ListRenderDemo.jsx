c233fe8210e855aca86f3ae20322237b5ef92d1b
MS-Raunak <raunakkr.saha@gmail.com>
2024-07-17 23:13:21 +0530

    key in list rendering in react

--- 01basic/src/list-rendering/ListRenderDemo.jsx
+++ 01basic/src/list-rendering/ListRenderDemo.jsx
@@ -6,10 +6,20 @@ function ListRenderDemo() {
   return (
     <>
       {
-        numberList.map((num, i) => <p key={i}>{num}</p>)
+        numberList.map((num, idx) => <p key={idx}>{num}</p>)
       }
     </>
   )
 }
 
 export default ListRenderDemo
+/**
+ * A key is a special list attribute which we use while creating the list.
+ * Keys help React identify which items changed or added or removed.
+ * It helps in efficient update of the UI
+ * 
+ * When to used index as key?
+ * When the list does not have any unique id.
+ * 
+ * Note: using index as key is not recommended.
+ */
--- 01basic/src/list-rendering/Person.jsx
+++ 01basic/src/list-rendering/Person.jsx
@@ -5,9 +5,20 @@ function PersonDisplayList({listOfPerson}) {
    
   return (
     <>
-      {listOfPerson.map((person, idx)=> <p key={idx}>My name is {person.name}, I am {person.age} year old and I am a {person.desig}</p>)}
+      {listOfPerson.map(person=> <p key={person.id}>My name is {person.name}, I am {person.age} year old and I am a {person.desig}</p>)}
     </>
   )
 }
 
 export default PersonDisplayList;
+
+/**
+ * A key is a special list attribute which we use while creating the list.
+ * Keys help React identify which items changed or added or removed.
+ * It helps in efficient update of the UI
+ * 
+ * When to used index as key?
+ * When the list does not have any unique id.
+ * 
+ * Note: using index as key is not recommended.
+ */

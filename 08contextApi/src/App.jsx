2f42478bc27059ccfeb67bd70bd696148856951d
MS-Raunak <raunakkr.saha@gmail.com>
2024-09-17 00:31:13 +0530

    contextAapi: part-1

---
+++ 08contextApi/src/App.jsx
@@ -0,0 +1,16 @@
+import UserContextProvider from './context/UserContextProvider'
+import './App.css'
+import Login from './component/Login'
+import Profile from './component/Profile'
+
+function App() {
+  return (
+    <UserContextProvider>
+      <h1>Context API</h1>
+      <Login />
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ 08contextApi/src/context/UserContext.js
@@ -0,0 +1,9 @@
+import {createContext} from 'react'
+
+const UserContext = createContext();
+
+export default UserContext;
+
+
+
+

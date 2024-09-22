463f92fc2a64e952325b5c0ae2bbd81b4912cfe0
MS-Raunak <raunakkr.saha@gmail.com>
2024-09-22 22:15:19 +0530

    redux: reducer

--- 11Redux-demo/index.js
+++ 11Redux-demo/index.js
@@ -1,8 +1,28 @@
 const CAKE_ORDERED = 'CAKE_ORDERED';
 
+//action creater function
 function orderCake() {
+    //return action object
     return {
         type : CAKE_ORDERED,
         quantity:1
     }
 }
+
+const initialState = {
+    numOfCakes: 10
+}
+
+// (previousState, action) => newState: reducer syntax
+const reducer = (state=initialState, action) => {
+    switch (action.type) {
+        case CAKE_ORDERED:
+            return{
+                ...state,
+                numOfCakes: state.numOfCakes-1
+            }
+        default:
+            return state;
+    }
+}
+

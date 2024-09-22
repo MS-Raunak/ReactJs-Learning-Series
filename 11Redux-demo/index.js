27209d1f68e29e9432534eceaf9b3ef7bf505ed7
MS-Raunak <raunakkr.saha@gmail.com>
2024-09-22 22:51:19 +0530

    update state

--- 11Redux-demo/index.js
+++ 11Redux-demo/index.js
@@ -1,3 +1,8 @@
+const redux = require('redux');
+
+const createStore = redux.createStore
+
+
 const CAKE_ORDERED = 'CAKE_ORDERED';
 
 //action creater function
@@ -26,3 +31,32 @@ const reducer = (state=initialState, action) => {
     }
 }
 
+const store = createStore(reducer)
+console.log('initial state ', store.getState());
+
+const unsubscribe = store.subscribe(()=>{
+    console.log('Updated state ', store.getState());
+    
+})
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe();
+
+
+/**
+ * Explanation:
+ * consider there is a cake shop app,
+ * 1-> user order is an action
+ * 2-> cake shop is store
+ * 3-> shopkeeper is reducer
+ * 
+ * In above code,
+ * we are creating action using a action creater function orderCake().
+ * (action returened in the form of object).
+ * suppose our total cake(initial state) is 10 in shop
+ * when user order a cake then shopkeeper as a reducer update the cake(9)
+ * by taking initialState as an arg.
+ */

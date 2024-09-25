4fc963e390a4b3cad73b316e0a35ba3d1000825f
MS-Raunak <raunakkr.saha@gmail.com>
2024-09-25 18:49:44 +0530

    redux configuration

--- mega-blog-project/src/App.jsx
+++ mega-blog-project/src/App.jsx
@@ -1,14 +1,42 @@
+import { useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
-import config from './config/config';
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import {Outlet} from 'react-router-dom'
 
 function App() {
-  console.log(config.appWriteUrl );
+  const[loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}));
+      }
+      else{
+        dispatch(logout());
+      }
+    })
+    .finally(() => setLoading(false))
+
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+         <main>
+          TODO:<Outlet/>
+         </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null;
   
-  return (
-    <>
-      <h2>Started</h2>
-    </>
-  )
 }
 
 export default App
--- mega-blog-project/src/appwrite/auth.js
+++ mega-blog-project/src/appwrite/auth.js
@@ -59,26 +59,3 @@ export class AuthService {
 const authService = new AuthService();
 
 export default authService;
-
-
-
-
-
-
-
-
-
-
-
-
-const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-    .setProject('<PROJECT_ID>');                 // Your project ID
-
-const account = new Account(client);
-
-const user = await account.create(
-    ID.unique(), 
-    'email@example.com', 
-    'password'
-);
